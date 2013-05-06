94835b06c5421f8a101218efad24978cfb475b9b
Harshac <harshac@thoughtworks.com>
2013-05-06 15:53:12 +0530

    MIFOSX-69 Fetching S3 details from database

--- mifosng-provider/src/main/java/org/mifosplatform/infrastructure/core/service/DocumentStoreFactory.java
+++ mifosng-provider/src/main/java/org/mifosplatform/infrastructure/core/service/DocumentStoreFactory.java
@@ -1,26 +1,29 @@
 package org.mifosplatform.infrastructure.core.service;
 
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import org.mifosplatform.infrastructure.configuration.domain.ConfigurationDomainService;
+import org.mifosplatform.infrastructure.configuration.domain.ExternalServicesDomainService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DocumentStoreFactory {
 
-    private ConfigurationDomainService configurationDomainServiceJpa;
+    private ApplicationContext applicationContext;
 
     @Autowired
-    public DocumentStoreFactory(ConfigurationDomainService configurationDomainService) {
-        this.configurationDomainServiceJpa = configurationDomainService;
+    public DocumentStoreFactory(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
     }
 
     public DocumentStore getInstance(){
         DocumentStore documentStore;
-        if(this.configurationDomainServiceJpa.isAmazonS3Enabled()){
-            AmazonS3Client s3Client = new AmazonS3Client(new BasicAWSCredentials("access_key", "secret_key"));
-            documentStore = new S3DocumentStore("bucket_name", s3Client);
+        ConfigurationDomainService configurationDomainServiceJpa = applicationContext.getBean("configurationDomainServiceJpa", ConfigurationDomainService.class);
+        if(configurationDomainServiceJpa.isAmazonS3Enabled()){
+            AmazonS3Client s3Client = applicationContext.getBean("s3ClientFactory", S3ClientFactory.class).getS3Client();
+            ExternalServicesDomainService externalServicesDomainService = applicationContext.getBean("externalServicesDomainService", ExternalServicesDomainService.class);
+            documentStore = new S3DocumentStore(externalServicesDomainService.getValue("s3_bucket_name"), s3Client);
         }
         else {
             documentStore = new FileSystemDocumentStore();
--- mifosng-provider/src/test/java/org/mifosplatform/infrastructure/core/service/DocumentStoreFactoryTest.java
+++ mifosng-provider/src/test/java/org/mifosplatform/infrastructure/core/service/DocumentStoreFactoryTest.java
@@ -4,7 +4,9 @@ package org.mifosplatform.infrastructure.core.service;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mifosplatform.infrastructure.configuration.domain.ConfigurationDomainService;
+import org.mifosplatform.infrastructure.configuration.domain.ExternalServicesDomainService;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.context.ApplicationContext;
 
 import static junit.framework.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -17,10 +19,23 @@ public class DocumentStoreFactoryTest {
 
     @Test
     public void shouldReturnS3DocumentStoreObjectIfS3Enabled(){
-        ConfigurationDomainService configurationDomainService = mock(ConfigurationDomainService.class);
-        when(configurationDomainService.isAmazonS3Enabled()).thenReturn(true);
+        S3ClientFactory s3ClientFactoryMock = mock(S3ClientFactory.class);
+        when(s3ClientFactoryMock.getS3Client()).thenReturn(null);
 
-        DocumentStoreFactory documentStoreFactory= new DocumentStoreFactory(configurationDomainService);
+        ExternalServicesDomainService externalServicesDomainServiceMock = mock(ExternalServicesDomainService.class);
+        when(externalServicesDomainServiceMock.getValue("s3_bucket_name")).thenReturn("bucket_name");
+
+        ConfigurationDomainService configurationDomainServiceMock = mock(ConfigurationDomainService.class);
+        when(configurationDomainServiceMock.isAmazonS3Enabled()).thenReturn(true);
+
+        ApplicationContext applicationContextMock = mock(ApplicationContext.class);
+
+        when(applicationContextMock.getBean("configurationDomainServiceJpa", ConfigurationDomainService.class)).thenReturn(configurationDomainServiceMock);
+        when(applicationContextMock.getBean("s3ClientFactory", S3ClientFactory.class)).thenReturn(s3ClientFactoryMock);
+        when(applicationContextMock.getBean("externalServicesDomainService", ExternalServicesDomainService.class)).thenReturn(externalServicesDomainServiceMock);
+
+
+        DocumentStoreFactory documentStoreFactory= new DocumentStoreFactory(applicationContextMock);
         assertEquals(documentStoreFactory.getInstance().getClass(),S3DocumentStore.class);
     }
 
@@ -29,7 +44,10 @@ public class DocumentStoreFactoryTest {
         ConfigurationDomainService configurationDomainService = mock(ConfigurationDomainService.class);
         when(configurationDomainService.isAmazonS3Enabled()).thenReturn(false);
 
-        DocumentStoreFactory documentStoreFactory= new DocumentStoreFactory(configurationDomainService);
+        ApplicationContext applicationContext = mock(ApplicationContext.class);
+        when(applicationContext.getBean("configurationDomainServiceJpa", ConfigurationDomainService.class)).thenReturn(configurationDomainService);
+
+        DocumentStoreFactory documentStoreFactory= new DocumentStoreFactory(applicationContext);
         assertEquals(documentStoreFactory.getInstance().getClass(),FileSystemDocumentStore.class);
     }
 
