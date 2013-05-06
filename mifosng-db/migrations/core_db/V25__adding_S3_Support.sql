48ba3a1b1ea1cdbf5e6c656147321ce6c347b080
Harshac <harshac@thoughtworks.com>
2013-05-06 15:19:32 +0530

    MIFOSX-69 Changing table s3_details to generic tabe for external services, adding column to store the storage type in m_documents table

--- mifosng-db/migrations/core_db/V25__adding_S3_Support.sql
+++ mifosng-db/migrations/core_db/V25__adding_S3_Support.sql
@@ -1,13 +1,15 @@
 INSERT INTO c_configuration (name,enabled) VALUES ('amazon-S3',0);
 
 
-CREATE TABLE IF NOT EXISTS m_s3_details (
+CREATE TABLE IF NOT EXISTS m_external_services (
 	name VARCHAR(150) NOT NULL,
 	value VARCHAR(250),
 	UNIQUE(name) 
 );
 
-INSERT INTO m_s3_details (name) VALUES ('cdn_url');
-INSERT INTO m_s3_details (name) VALUES ('bucket_name');
-INSERT INTO m_s3_details (name) VALUES ('access_key');
-INSERT INTO m_s3_details (name) VALUES ('secret_key');
+INSERT INTO m_external_services (name) VALUES ('s3_cdn_url');
+INSERT INTO m_external_services (name) VALUES ('s3_bucket_name');
+INSERT INTO m_external_services (name) VALUES ('s3_access_key');
+INSERT INTO m_external_services (name) VALUES ('s3_secret_key');
+
+ALTER TABLE m_document ADD COLUMN storage_type varchar(50);
